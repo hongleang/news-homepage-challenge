1a9594e92750a5db6760e6257abda6302b240c10
Hongleang Lim <lim.hongleang@gmail.com>
2023-03-01 17:54:02 +1030

    fix landmark

--- src/components/feature/Features.tsx
+++ src/components/feature/Features.tsx
@@ -11,7 +11,7 @@ export default function Features() {
     if (featuringNews.length === 0) return <>Errors!! No Feature news</>
 
     return (
-        <section role="featuring" id="featuring-news" className='container' style={{ marginTop: 70 }}>
+        <section role="banner" id="featuring-news" className='container' style={{ marginTop: 70 }}>
             <div className="row row-cols-1 row-cols-md-3 justify-content-center justify-content-md-between" style={
                 mobileSize ? {
                     paddingLeft: 20
